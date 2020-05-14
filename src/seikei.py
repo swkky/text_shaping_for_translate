1a5bfdac6eb5c71c32495b5bedfa7c5a91626470
Atsuto Ishinaga <atsutoishinaga@isb2-dhcp-170-212.naist.jp>
2020-05-14 17:59:39 +0900

    change seikei.py

--- src/seikei.py
+++ src/seikei.py
@@ -10,6 +10,9 @@ def reshape_text(text):
     ret = re.sub(r"\r\n", " ", ret)
     #ret = re.sub(r"-", "", ret)
     ret = ret.replace('et al.','et al')
+    ret = ret.replace('i.e.','i e ')
+    ret = ret.replace('e.g.','e g ')
+    ret = ret.replace('Fig.','Fig ')
     ret = ret.replace('.”','.”\r\n')
     #.(ピリオド)で終わるが，直後に一桁以上の数字+%が続かない.(ピリオド)を改行に置換
     #82.1%などの対策|2.2など
